3118c583b7737ec9a285d5ed2b74f5c4701deb31
LimeShot <il.chumackov@yandex.ru>
2023-05-09 23:23:52 +0700

    maximal_cringe

--- lab4/pv4.for
+++ lab4/pv4.for
@@ -1,61 +1,76 @@
       program IntegralCalc
         Implicit NONE
-        integer len, i
-        real rgrid(30), rectcringe, RectCalc, simpcringe, SimpCalc
-        real gausscringe, GaussCalc
-        open(1,file = 'cringeinput.txt')
-        read(1,*) len
-        do i = 1, len
-           read(1,*) rgrid(i)
-        end do
-        rectcringe = RectCalc(rgrid,len)
-        simpcringe = SimpCalc(rgrid,len)
-        gausscringe = GaussCalc(rgrid,len)
+        integer N
+        real grid(1000000), rectcringe, RectCalc, simpcringe, SimpCalc
+        real gausscringe, GaussCalc, a, b
+        read*,N, a, b
+        call GridCreate(grid, a, b, N)
+        rectcringe = RectCalc(grid,N+1)
+        print*,'Rectangle',rectcringe
+        simpcringe = SimpCalc(grid,N+1)
+        print*,'Simpson', simpcringe
+        gausscringe = GaussCalc(grid,N+1)
+        print*,'Gauss',gausscringe
       end
 
-      function RectCalc(rgrid,len)
+      function RectCalc(grid,len)
         Implicit NONE
         integer len, i
-        real rgrid(30), fun, RectCalc  
+        real grid(1000000), fun, RectCalc  
         RectCalc = 0
         do i = 1, len-1
-          RectCalc = RectCalc+(rgrid(i+1)-rgrid(i))*
-     >fun((rgrid(i+1)-rgrid(i))/2.0)
+          RectCalc = RectCalc+(grid(i+1)-grid(i))*
+     >fun((grid(i+1)+grid(i))/2.0)
         end do
       end 
 
-      function SimpCalc(rgrid,len)
+      function SimpCalc(grid,len)
         Implicit NONE
         integer len, i
-        real rgrid(30), fun, SimpCalc
-        SimpCalc = 1.0/6.0*(fun(rgrid(1))*(rgrid(2)-rgrid(1))+
-     >fun(rgrid(len))*(rgrid(len)-rgrid(len-1)))
+        real grid(1000000), fun, SimpCalc
+        SimpCalc = (fun(grid(1))*(grid(2)-grid(1))+
+     >fun(grid(len))*(grid(len)-grid(len-1)))
         do i = 1, len-1
-          SimpCalc = SimpCalc+(rgrid(i+1)-rgrid(i))*
-     >fun((rgrid(i+1)-rgrid(i))/2.0)
+          SimpCalc = SimpCalc+4.0*(grid(i+1)-grid(i))*
+     >fun((grid(i+1)+grid(i))/2.0)
         end do
         do i = 1, len-2
-            SimpCalc = SimpCalc+(rgrid(i+2)-rgrid(i))*
-     >fun((rgrid(i+1)))
-          end do
+            SimpCalc = SimpCalc+(grid(i+2)-grid(i))*
+     >fun((grid(i+1)))
+        end do
+        SimpCalc = 1.0/6.0*SimpCalc
       end
 
-      function GaussCalc(rgrid,len)
+      function GaussCalc(grid,len)
         Implicit NONE
         integer len, i,k
-        real rgrid(30), fun, GaussCalc, root(2), x
-        root(1) = (1/sqrt(3.0))
-        root(2) = (-1/sqrt(3.0))
+        real grid(1000000), fun, GaussCalc, root(2), x
+        root(1) = (1.0/sqrt(3.0))
+        root(2) = (-1.0/sqrt(3.0))
         do i = 1, 2
             do k=1,len-1
-                x =(rgrid(k+1)+rgrid(k))/2.0+root(i)*
-     >(rgrid(k+1)-rgrid(k))/2.0
-            GaussCalc = GaussCalc+fun(x)*(rgrid(k+1)-rgrid(k))
+                x =(grid(k+1)+grid(k))/2.0+root(i)*
+     >(grid(k+1)-grid(k))/2.0
+            GaussCalc = GaussCalc+fun(x)*(grid(k+1)-grid(k))
             end do
         end do
         GaussCalc = GaussCalc/2.0
       end
 
       function fun(x)
-        fun = x
+        real x
+        fun = 5**(-0.5*x)*cos(8.0*x)
       end
+
+      subroutine GridCreate(grid, a, b, N)
+        Implicit NONE
+        integer N, i
+        real h, a, b
+        real grid(100000)
+        h = (b-a)/real(N)
+        print*, 'h:', h
+        do i = 1, N
+            grid(i+1)=a+h*real(i)
+        end do
+        grid(1)=a
+      end 
